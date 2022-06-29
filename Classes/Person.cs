b60ba85d7729ea0776265fdcc3d81df856d68f1b
Ng_Le <Ng_Le@DESKTOP-KOPIKPM>
2022-06-29 23:21:51 +1000

    Add project files.

---
+++ Classes/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    
+    
+        public class Person
+        {
+            public string Name { get; set; }
+            public char Sex { get; set; }
+            public int Age { get; set; }
+            public int Height { get; set; }
+            public float Weight { get; set; }
+
+            public Person()
+            { }
+
+        public Person(string n, char s, int a, int h, float w)
+        {
+            Name = n;
+            Sex = s;
+            Age = a;
+            Height = h;
+            Weight = w;
+        }
+
+        }
+    
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+using Classes;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+
+
+
+
+using (var reader = new StreamReader("C:\\Users\\Ng_Le\\source\\repos\\Classes\\Classes\\biostats2.csv"))
+using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+{
+    var records = new List<Person>();
+    csv.Read();     
+    csv.ReadHeader();
+    while (csv.Read())
+    {
+        var record = new Person()
+        {
+            Name = csv.GetField("Name").ToString(),
+            Sex = char.Parse(csv.GetField("Sex").ToString()),
+            Age = int.Parse(csv.GetField("Age").ToString()),
+            Height = int.Parse(csv.GetField("Height").ToString()),
+            Weight = float.Parse(csv.GetField("Weight").ToString())
+        };
+        records.Add(record);
+    }
+
+
+    for (int i = 0; i < records.Count; i++)
+        Console.WriteLine(records[i].Name);
+
+
+
+}
+
+
